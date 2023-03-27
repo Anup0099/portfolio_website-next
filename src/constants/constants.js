2843f2200093ca35f96171b18ed04b47e3e310c5
anup <anuppal602@gmail.com>
2023-03-27 18:22:28 +0530

    done

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,17 +1,28 @@
- export const projects = [
+export const projects = [
+  {
+    title: 'Study Verse',
+    description: ' Study Verse is an online learning platform.We have used React with Tailwind to implement the frontend part and NodeJS, MongoDB for the backend part ',
+    image: '/images/study.jpg',
+    tags: [' React', 'Css', 'MongoDB', 'Express', 'Nodejs'],
+    source: 'https://github.com/Anup0099/portfolio_website-next',
+    visit: 'https://github.com/Anup0099/portfolio_website-next',
+    id: 0,
+  },
 
   {
     title: 'Restaurant App',
-    description: "Using React, Css.this app is a restaurant app that allows users to order food and also view the menu.Any restaurant can use this app to move their  buisness online.",
-      image: '/images/re.gif',
-      tags: [' React', 'Css'],
+    description:
+      'Using React, Css.this app is a restaurant app that allows users to order food and also view the menu.Any restaurant can use this app to move their  buisness online.',
+    image: '/images/re.gif',
+    tags: [' React', 'Css'],
     source: 'https://github.com/Anup0099/Restaurant',
     visit: 'https://ayanrestaurantsite.netlify.app/#home',
     id: 0,
   },
   {
     title: 'Tesla-App',
-    description:"Using React, Css, and Axios, this app is a Tesla app that allows users to view the Tesla cars and also to order them.Any user can use this app to order the Tesla cars.",
+    description:
+      'Using React, Css, and Axios, this app is a Tesla app that allows users to view the Tesla cars and also to order them.Any user can use this app to order the Tesla cars.',
     image: '/images/tesla.gif',
     tags: ['React', 'JavaScript'],
     source: 'https://github.com/Anup0099/Tesla-Clone',
@@ -20,27 +31,34 @@
   },
   {
     title: 'Medical Pager',
-    description: "It is a app for consulting among doctors.It is a pager app that allows users to consult among doctors.",
-      image: '/images/medical.gif',
-      tags: ['React', 'Nodejs'],
+    description:
+      'It is a app for consulting among doctors.It is a pager app that allows users to consult among doctors.',
+    image: '/images/medical.gif',
+    tags: ['React', 'Nodejs'],
     source: 'https://github.com/DSN-2/medical-pager',
     visit: 'medical-pager-2022.netlify.app/',
     id: 2,
   },
   {
     title: 'rock-paper-scissors',
-    description: "It is a game that allows users to play rock paper scissors with each other.",
+    description:
+      'It is a game that allows users to play rock paper scissors with each other.',
     image: '/images/rockpaper.gif',
     tags: ['JavaScript', 'css'],
     source: 'https://github.com/Anup0099/Rock-Paper-Scissors',
     visit: 'gallant-spence-add7f5.netlify.app/',
     id: 3,
   },
-];
- 
+]
 
 export const TimeLineData = [
-   { year: 2020, text: 'Started my journey', },
-  { year: 2021, text: 'Learned HTML , CSS and JavaScript and made some basic projects', },
-  { year: 2022, text:'Learned React.js and Node.js and made some full stack projects', },
-];
+  { year: 2020, text: 'Started my journey' },
+  {
+    year: 2021,
+    text: 'Learned HTML , CSS and JavaScript and made some basic projects',
+  },
+  {
+    year: 2022,
+    text: 'Learned React.js and Node.js and made some full stack projects',
+  },
+]
