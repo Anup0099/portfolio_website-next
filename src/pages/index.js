ea8af27636778cd640e8f596a02ac1876a1a716b
anup <anuppal602@gmail.com>
2023-03-27 19:34:42 +0530

    done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Acomplishments from "../components/Acomplishments/Acomplishments";
 import BgAnimation from "../components/BackgrooundAnimation/BackgroundAnimation";
-import ContactForm from "../components/Contact/ContactForm";
+
 import Hero from "../components/Hero/Hero";
 import Projects from "../components/Projects/Projects";
 import Technologies from "../components/Technologies/Technologies";
